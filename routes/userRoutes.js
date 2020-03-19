22f65e92077ec4e597d5fd22e509a3112f9a13e6
Abdul Khaliq <31276822+makflakes@users.noreply.github.com>
2020-03-20 00:39:31 +0530

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,14 +5,14 @@ module.exports = function(app) {
 
 var product = require('../controllers/userController');
 app.route('/rest/v1/Users')
-    .get(product.list_all_users)
-    .post(product.create_a_user);
+    .get(product.showusers)
+    .post(product.createnewuser);
 
 
 app.route('/rest/v1/Users/:username')
-    .get(product.read_a_user)
-    .put(product.update_a_user)
-    .delete(product.delete_a_user);
+    .get(product.userdetails)
+    .put(product.updateuser)
+    .delete(product.deleteuser);
 
 
 
