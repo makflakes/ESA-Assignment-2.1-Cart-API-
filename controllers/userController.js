b008b0fcb61a76f09fba53fc4c7d36407d90e9ed
Abdul Khaliq <31276822+makflakes@users.noreply.github.com>
2020-03-20 00:35:29 +0530

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -9,7 +9,7 @@ User = mongoose.model('User');
 
 
 
-exports.list_all_users = function(req, res) {
+exports.showusers = function(req, res) {
   User.find({}, function(err, user) {
     if (err)
       res.send(err);
@@ -20,9 +20,9 @@ exports.list_all_users = function(req, res) {
 
 
 
-exports.create_a_user = function(req, res) {
-  var new_user = new User(req.body);
-  new_user.save(function(err, user) {
+exports.createnewuser = function(req, res) {
+  var newuser = new User(req.body);
+  newuser.save(function(err, user) {
     if (err)
       res.send(err);
     res.json(user);
@@ -30,7 +30,7 @@ exports.create_a_user = function(req, res) {
 };
 
 
-exports.read_a_user = function(req, res) {
+exports.userdetails = function(req, res) {
   User.find({username:req.params.username}, function(err, user) {
     if (err)
       res.send(err);
@@ -39,7 +39,7 @@ exports.read_a_user = function(req, res) {
 };
 
 
-exports.update_a_user = function(req, res) {
+exports.updateuser = function(req, res) {
   User.findOneAndUpdate({username: req.params.username}, req.body, {new: true}, function(err, user) {
     if (err)
       res.send(err);
@@ -48,12 +48,10 @@ exports.update_a_user = function(req, res) {
 };
 
 
-exports.delete_a_user = function(req, res) {
-
-
-  User.remove({
-    username: req.params.username
-  }, function(err, product) {
+exports.deleteuser = function(req, res) {
+User.remove({
+    username: req.params.username},
+     function(err, product) {
     if (err)
       res.send(err);
     res.json({ message: 'Product successfully deleted' });
