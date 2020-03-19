7d9d6eec4f106a4ac7f9b232d211c95b732ac7a3
Abdul Khaliq <31276822+makflakes@users.noreply.github.com>
2020-03-20 00:39:05 +0530

    Update productRoutes.js

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -5,14 +5,14 @@ var product = require('../controllers/productController');
 
 // todoList Routes
 app.route('/rest/v1/products')
-.get(product.list_all_products)
-.post(product.create_a_product);
+.get(product.allproducts)
+.post(product.createnewproduct);
 
 
 app.route('/rest/v1/product/:productId')
-.get(product.read_a_product)
-.put(product.update_a_product)
-.delete(product.delete_a_product);
+.get(product.showproduct)
+.put(product.updateproduct)
+.delete(product.deleteproduct);
 
 
 
